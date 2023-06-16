6b92e91f7593acb2b93fbc3ff876cc2d06a829a6
hboppe <hanna.nascimento31@gmail.com>
2023-06-16 09:10:38 -0400

    feat: add client field to Movie Model and add MovieOrder model

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from users.models import User
+
 
 class Ratings(models.TextChoices):
     DEFAULT = "G"
@@ -22,3 +24,29 @@ class Movie(models.Model):
         on_delete=models.CASCADE,
         related_name="movies"
     )
+
+    clients = models.ManyToManyField(
+        User,
+        through="MovieOrder",
+        related_name="ordered_movies_by_user"
+    )
+
+
+class MovieOrder(models.Model):
+    buyed_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2
+    )
+    ordered_movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_orders"
+    )
+
+    client = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="orders"
+    ) 
+    
