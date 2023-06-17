13603652952fa2867dac8b4c185626f2f3e047e2
hboppe <hanna.nascimento31@gmail.com>
2023-06-16 20:15:36 -0400

    refactor: add pagination to GET movies

--- movies/views.py
+++ movies/views.py
@@ -5,18 +5,20 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from users.permissions import IsAdminOrReadOnly
 from django.shortcuts import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
 import ipdb
 
 
-class MovieView(APIView):
+class MovieView(APIView, PageNumberPagination):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAdminOrReadOnly]
 
     def get(self, request: Request) -> Response:
         movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
+        result_page = self.paginate_queryset(movies, request, view=self)
+        serializer = MovieSerializer(result_page, many=True)
 
-        return Response(serializer.data)
+        return self.get_paginated_response(serializer.data)
     
     def post(self, request: Request) -> Response:
         serializer = MovieSerializer(data=request.data)
