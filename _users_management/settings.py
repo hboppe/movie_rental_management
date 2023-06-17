7fed8c10c8065f74eefab0c564e5198a6f824591
hboppe <hanna.nascimento31@gmail.com>
2023-06-16 20:15:49 -0400

    chore: add pagination

--- _users_management/settings.py
+++ _users_management/settings.py
@@ -51,6 +51,11 @@ DJANGO_APPS = [
 
 INSTALLED_APPS = THIRD_PARTY_APPS + MY_APPS + DJANGO_APPS 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
