b655324bc37b2e25575fbe7f6ca4c99c9865ed21
hboppe <hanna.nascimento31@gmail.com>
2023-06-16 20:19:27 -0400

    feat: add UserDetailView

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView, Request, Response, status
 from users.serializers import UserSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .permissions import IsAdminOrAccountOwner, IsAdminOrReadOnly
+from django.shortcuts import get_object_or_404
+from .models import User
 
 
 class UserView(APIView):
@@ -10,3 +14,23 @@ class UserView(APIView):
         serializer.save()
 
         return Response(serializer.data, status.HTTP_201_CREATED)
+    
+
+class UserDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminOrAccountOwner]
+
+    def get(self, request: Request, user_id: int) -> Response:
+        user = get_object_or_404(User, id=user_id)
+        self.check_object_permissions(request, user)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def patch(self, request: Request, user_id: int) -> Response:
+        user = get_object_or_404(User, id=user_id)
+        self.check_object_permissions(request, user)
+
+        serializer = UserSerializer(user, request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
